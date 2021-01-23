6e5666241a620ddc960cbc4bce2b7d0cfa8ff347
Sara <sara.rojas@qindel.com>
2021-01-23 13:16:32 +0100

    fix fetch problems and add some styles

--- src/App.js
+++ src/App.js
@@ -1,35 +1,35 @@
 import "./App.css";
 import getImages from "./services/getImages";
-import { useState } from "react";
+import { useState, Fragment, useEffect } from "react";
 
 function App() {
   const [gifs, setGifs] = useState([]);
-  const keyword = "poker";
 
-  const apiKey = "oHaXvTbitKxXjIh3JxwJLoNZ4tYRsXCb";
-  const apiUrl = `https://api.giphy.com/v1/gifs/search?api_key=${apiKey}&q=${keyword}&limit=25&offset=0&rating=g&lang=en`;
+  useEffect(() => {
+      const apiKey = "oHaXvTbitKxXjIh3JxwJLoNZ4tYRsXCb";
+      const keyword = 'morty';
+      const apiUrl = `https://api.giphy.com/v1/gifs/search?api_key=${apiKey}&q=${keyword}&limit=10&offset=0&rating=g&lang=en`;
 
-  fetch(apiUrl)
-    .then((response) => response.json())
-    .then((response) => {
-      const { data } = response;
-      const id = data.map((image) => image.id);
-      const gifs = data.map((image) => image.embed_url);
-      setGifs(gifs);
-    });
+      fetch(apiUrl)
+        .then(response => response.json())
+        .then(responseData => {
+            const {data} = responseData;
+            const gifs = data.map((image) => image.images.original.url);
+            setGifs(gifs);
+    })})
+
   return (
-    <div>
-      {gifs.map((gif, id) => {
-        console.log(gif);
-        return (
-          <section>
-            <h2>Soy un gif</h2>
-            <img src="{gif}"></img>
-          </section>
-        );
-      })}
-    </div>
-  );
-}
+      <Fragment>
+          <div class="app-container">
+              {
+                  gifs.map(image => {
+                      return <img src={image}></img>
+                  })
+              }
+          </div>
+      </Fragment>
+  )
+  
+}  
 
 export default App;
